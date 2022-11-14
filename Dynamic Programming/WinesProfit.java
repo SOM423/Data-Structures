0668c87e2eecfdfd3598d39957e87febf22a710c
SOM423 <75077641+SOM423@users.noreply.github.com>
2022-11-14 22:37:47 +0530

    Calculate Maximum Profit by wines

--- Dynamic Programming/WinesProfit.java
+++ Dynamic Programming/WinesProfit.java
@@ -2,12 +2,15 @@ public class WinesProblem {
 	
 	public static void main(String[] args) {
 		
-		int[][] dp = new int[1000][1000];
-		for(int i=0;i<1000;i++)
+		int[][][] dp = new int[100][100][101];
+		for(int i=0;i<100;i++)
 		{
-			for(int j=0;j<1000;j++)
+			for(int j=0;j<100;j++)
 			{
-				dp[i][j]=-1;
+				for(int k=0;k<101;k++)
+				{
+					dp[i][j][k]=-1;
+				}
 			}
 		}
 			Scanner s = new Scanner(System.in);
@@ -18,15 +21,15 @@ public class WinesProblem {
 			System.out.println(profit(arr,0,N-1,1,dp));
 		}
 		
-		public static int profit(int[] arr,int l,int r,int year,int[][] dp)
+		public static int profit(int[] arr,int l,int r,int year,int[][][] dp)
 		{
 		    if(year > arr.length) return 0;
 		    
-		    if(dp[l][r] != -1) return dp[l][r];
+		    if(dp[l][r][year] != -1) return dp[l][r][year];
 		    int pr1=(arr[l] * year) + profit(arr,l+1,r,year+1,dp);
 		    int pr2=(arr[r] * year) + profit(arr,l,r-1,year+1,dp);
 		    
-		    return dp[l][r]=Math.max(pr1,pr2);
+		    return dp[l][r][year]=Math.max(pr1,pr2);
 		}
 
 }
