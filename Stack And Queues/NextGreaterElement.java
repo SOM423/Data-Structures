5324359758f397bff4d2ad6c3e2cb47f6ad826f3
SOM423 <75077641+SOM423@users.noreply.github.com>
2022-11-26 22:02:55 +0530

    Next Greater Element Using Monotone Satck

--- Stack And Queues/NextGreaterElement.java
+++ Stack And Queues/NextGreaterElement.java
@@ -1,44 +1,26 @@
-
-public class NextGreaterElement {
-
-	public static void main(String[] args) {
-		int[] abc= new int[] {1,3,2,4,5};
-		int[] arr = canSeePersonsCount(abc);
-		
-		for(int i : arr)
-		{
-			System.out.println(i);
-		}
-	}
-	
-	public static int[] canSeePersonsCount(int[] A) {
-		int n= A.length;
-        int[] result = new int[n];
-		Stack<Integer> stack = new Stack<>();
+class Solution
+{
+    //https://practice.geeksforgeeks.org/problems/next-larger-element-1587115620/1
     
-    //Iterating loop from last element
-		for(int i=n-1;i>=0;i--)
-		{
-			if(!stack.isEmpty())
-			{
-				while(!stack.isEmpty() && stack.peek() <= A[i])
-				{
-					stack.pop();
-				}
-			}
-			if(stack.isEmpty())
-			{
-				result[i] =-1;
-			}
-			else {
-				result[i] = stack.peek();
-			}
-			stack.push(A[i]);
-			
-		}
-		
-		return result;
-		
-
-}
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+       
+        
+       long[] ans = new long[arr.length];
+       Arrays.fill(ans,-1);
+       
+       Stack<Integer> stack = new Stack<>();
+       
+       for(int i=0;i<arr.length;i++)
+       {
+           while(stack.size() !=0 && arr[stack.peek()] < arr[i])
+           {
+               ans[stack.peek()] = arr[i];
+               stack.pop();
+           }
+           
+           stack.push(i);
+       }
+        return ans;
+    } 
 }
